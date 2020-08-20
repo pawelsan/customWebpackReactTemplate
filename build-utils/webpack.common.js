66262c3abd57b60c15ac875086985bed8ee3bb24
pawelsan <pawelsan@yahoo.com>
2020-08-20 13:34:26 +0200

    implement css modules

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,40 +1,53 @@
-const path = require('path');
-const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    // 1
-    entry: './src/index.js',
-    module: {
-        rules: [
+  // 1
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
           {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-          }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    // 2
-    output: {
-      path: path.resolve(__dirname, '../', 'dist'),
-      publicPath: '/',
-      filename: 'bundle.js'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-          title: 'Custom React Project',
-          template: './src/index.html'
-        })
-      ],
-    // 3
-    devServer: {
-      contentBase: './dist',
-      hot: true
-    }
-  };
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  // 2
+  output: {
+    path: path.resolve(__dirname, "../", "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Custom React Project",
+      template: "./src/index.html",
+    }),
+  ],
+  // 3
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
+import styles from "./style.css";
 
-const App = ({title}) => {
-    return ( 
-        <div>{title}
-        </div>
-     );
-}
- 
+const App = ({ title }) => {
+  return <div className={styles.title}>{title}</div>;
+};
+
 export default App;
