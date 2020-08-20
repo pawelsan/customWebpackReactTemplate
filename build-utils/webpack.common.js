cebe2eb4ddb390c5fd49e9274d638e321f9204d2
pawelsan <pawelsan@yahoo.com>
2020-08-20 12:47:19 +0200

    first commit

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // 1
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    // 2
+    output: {
+      path: path.resolve(__dirname, '../', 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+          title: 'Custom React Project',
+          template: './src/index.html'
+        })
+      ],
+    // 3
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    }
+  };
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({title}) => {
+    return ( 
+        <div>{title}
+        </div>
+     );
+}
+ 
+export default App;
